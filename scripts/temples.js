2560a0f934e9ce5559163e0134000f99189a623d
Iago Melo <iagomottamelo@gmail.com>
2025-09-15 15:13:02 -0400

    #

--- scripts/temples.js
+++ scripts/temples.js
@@ -1,15 +1,70 @@
 /* ========= Dataset (>= 9 itens) ========= */
 const temples = [
-    { name: "Salt Lake Utah Temple", location: "Salt Lake City, Utah, USA", dedicated: 1893, area: 382207, imageUrl: "https://content.churchofjesuschrist.org/templesldsorg/bc/Temples/photo-galleries/salt-lake-city-utah/2018/320x200/slctemple7.jpg" },
-    { name: "Nauvoo Illinois Temple", location: "Nauvoo, Illinois, USA", dedicated: 1846, area: 54000, imageUrl: "https://content.churchofjesuschrist.org/templesldsorg/bc/Temples/photo-galleries/nauvoo-illinois/320x200/nauvoo-temple-756499-wallpaper.jpg" },
-    { name: "Rome Italy Temple", location: "Rome, Italy", dedicated: 2019, area: 41010, imageUrl: "https://content.churchofjesuschrist.org/templesldsorg/bc/Temples/photo-galleries/rome-italy/2019/320x200/7-Rome-Temple-2160340.jpg" },
-    { name: "San Diego California Temple", location: "San Diego, California, USA", dedicated: 1993, area: 72000, imageUrl: "https://content.churchofjesuschrist.org/templesldsorg/bc/Temples/photo-galleries/san-diego-california/320x200/san-diego-mormon-temple-1164678-wallpaper.jpg" },
-    { name: "Gilbert Arizona Temple", location: "Gilbert, Arizona, USA", dedicated: 2014, area: 85326, imageUrl: "https://content.churchofjesuschrist.org/templesldsorg/bc/Temples/photo-galleries/gilbert-arizona/320x200/gilbert-arizona-temple-exterior-1207309-wallpaper.jpg" },
-    { name: "Paris France Temple", location: "Le Chesnay, France", dedicated: 2017, area: 44175, imageUrl: "https://content.churchofjesuschrist.org/templesldsorg/bc/Temples/photo-galleries/paris-france/320x200/paris-france-temple-exterior-1905503.jpg" },
-    { name: "Laie Hawaii Temple", location: "Laie, Hawaii, USA", dedicated: 1919, area: 42100, imageUrl: "https://content.churchofjesuschrist.org/templesldsorg/bc/Temples/photo-galleries/laie-hawaii/320x200/laie-temple-772761-wallpaper.jpg" },
-    { name: "Rio de Janeiro Temple", location: "Rio de Janeiro, BRA", dedicated: 2022, area: 29966, imageUrl: "https://content.churchofjesuschrist.org/templesldsorg/bc/Temples/photo-galleries/rio-de-janeiro-brazil/320x200/4-5aa85fb6f20a17f629302687710142328a707d4d.jpeg" },
-    { name: "Provo City Center Temple", location: "Provo, Utah, USA", dedicated: 2016, area: 85084, imageUrl: "https://content.churchofjesuschrist.org/templesldsorg/bc/Temples/photo-galleries/provo-city-center/2018/320x200/Provo-City-Center-Temple01.jpg" }
+    {
+        name: "Salt Lake Utah Temple",
+        location: "Salt Lake City, Utah, USA",
+        dedicated: 1893,
+        area: 382207,
+        imageUrl: "images/slctemple.jpg"
+    },
+    {
+        name: "Nauvoo Illinois Temple",
+        location: "Nauvoo, Illinois, USA",
+        dedicated: 1846,
+        area: 54000,
+        imageUrl: "images/nauvoo-temple.jpg"
+    },
+    {
+        name: "Rome Italy Temple",
+        location: "Rome, Italy",
+        dedicated: 2019,
+        area: 41010,
+        imageUrl: "images/Rome-Temple.jpg"
+    },
+    {
+        name: "San Diego California Temple",
+        location: "San Diego, California, USA",
+        dedicated: 1993,
+        area: 72000,
+        imageUrl: "images/san-diego-temple.jpg"
+    },
+    {
+        name: "Gilbert Arizona Temple",
+        location: "Gilbert, Arizona, USA",
+        dedicated: 2014,
+        area: 85326,
+        imageUrl: "images/gilbert-arizona-temple.jpg"
+    },
+    {
+        name: "Paris France Temple",
+        location: "Le Chesnay, France",
+        dedicated: 2017,
+        area: 44175,
+        imageUrl: "images/paris-france-temple.jpg"
+    },
+    {
+        name: "Laie Hawaii Temple",
+        location: "Laie, Hawaii, USA",
+        dedicated: 1919,
+        area: 42100,
+        imageUrl: "images/laie-temple.jpg"
+    },
+    {
+        name: "Rio de Janeiro Temple",
+        location: "Rio de Janeiro, BRA",
+        dedicated: 2022,
+        area: 29966,
+        imageUrl: "images/RJ-temple.jpeg"
+    },
+    {
+        name: "Provo City Center Temple",
+        location: "Provo, Utah, USA",
+        dedicated: 2016,
+        area: 85084,
+        imageUrl: "images/Provo-City-Center-Temple.jpg"
+    }
 ];
+
 
 /* ========= Utilidades ========= */
 const galleryEl = document.getElementById("gallery");
